b608d3bbd5a479347316d63049ff9e128d39f440
Sarvesh Tambe <sarveshtambe2011@gmail.com>
2026-02-06 11:32:48 +0000

    Try fixing this error: `Console Error: [GSI_LOGGER]: FedCM get() rejects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script src="https://accounts.google.com/gsi/client" async></script>
+        <script src="https://accounts.google.com/gsi/client" async defer></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
