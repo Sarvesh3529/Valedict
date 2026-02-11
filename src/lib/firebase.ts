d27335a82961dc4bb7fb9c36d9eca68c4d708df6
Sarvesh Tambe <sarveshtambe2011@gmail.com>
2026-02-11 11:43:29 +0000

    The measurment id is not there , i have entered everything else , what i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,6 +9,7 @@ const firebaseConfig: FirebaseOptions = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // Measurement ID is optional and used for Google Analytics
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
